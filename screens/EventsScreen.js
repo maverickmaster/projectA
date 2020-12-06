a865683d3368dfe1630886df83ed8eca68dbe4fa
milton <milton@miltons-MacBook.local>
2020-12-06 19:54:37 +0800

    Add Grand Slam pic

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Button, Image } from "react-native";
+import { View, Button, StyleSheet, Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import {
   Text,
@@ -15,7 +15,6 @@ import {
 function EventsHomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Welcome to my event!</Text>
       <Text
         style={{
           fontStyle: "italic",
@@ -26,14 +25,22 @@ function EventsHomeScreen({ navigation }) {
       >
         2021 Australian Open Tennis
       </Text>
-      <Text>Melbourne, Australia: January 18-31, 2021</Text>
-      <Image
-        source={{
-          uri:
-            "https://events.com.au/wp-content/uploads/2020/03/Australian-Open.png",
-        }}
-        style={{ height: 200, width: null, flex: 10 }}
-      />
+
+      <Card style={styles.card}>
+        <CardItem>
+          <Text>Melbourne, Australia: January 18-31, 2021 </Text>
+        </CardItem>
+        <CardItem cardBody>
+          <Image
+            source={{
+              uri:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Australian_Open_Logo_2017.svg/1200px-Australian_Open_Logo_2017.svg.png",
+            }}
+            style={{ height: 400, width: null, flex: 1 }}
+          />
+        </CardItem>
+      </Card>
+
       <Button
         title="Next Event- French Open"
         onPress={() => navigation.navigate("French Open")}
@@ -55,14 +62,22 @@ function EventsSecondScreen({ navigation }) {
       >
         2021 French Open Tennis
       </Text>
-      <Text>Roland Garros, France May 17 -June 6, 2021</Text>
-      <Image
-        source={{
-          uri:
-            "https://images.prismic.io/fft-rg-site/58ff2d89-3f3d-4e85-859f-7c89d50e2581_RG_log_inst_cou_R.png?auto=compress,format",
-        }}
-        style={{ height: 200, width: null, flex: 10 }}
-      />
+
+      <Card style={styles.card}>
+        <CardItem>
+          <Text>Roland Garros , France: May 17 -June 6, 2021</Text>
+        </CardItem>
+        <CardItem cardBody>
+          <Image
+            source={{
+              uri:
+                "https://yt3.ggpht.com/a/AATXAJy7xtmtPyeVc-JKfVz_yg7aLwVqU3TltRKNrCgjKw=s900-c-k-c0x00ffffff-no-rj",
+            }}
+            style={{ height: 400, width: null, flex: 10 }}
+          />
+        </CardItem>
+      </Card>
+
       <Button
         title="Next Event- Wimbledon Championship"
         onPress={() => navigation.navigate("Wimbledon")}
@@ -84,14 +99,20 @@ function EventsThirdScreen({ navigation }) {
       >
         2021 Wimbledon Championship Tennis
       </Text>
-      <Text>London, United Kingdom: 28 June – 11 July, 2021</Text>
-      <Image
-        source={{
-          uri:
-            "https://static.toiimg.com/thumb/msid-78699698,width-1200,height-900,resizemode-4/.jpg",
-        }}
-        style={{ height: 200, width: null, flex: 10 }}
-      />
+      <Card style={styles.card}>
+        <CardItem>
+          <Text>London, United Kingdom: 28 June – 11 July, 2021</Text>
+        </CardItem>
+        <CardItem cardBody>
+          <Image
+            source={{
+              uri:
+                "https://static.toiimg.com/thumb/msid-78699698,width-1200,height-900,resizemode-4/.jpg",
+            }}
+            style={{ height: 400, width: null, flex: 10 }}
+          />
+        </CardItem>
+      </Card>
       <Button
         title="Next Event- US Open"
         onPress={() => navigation.navigate("US Open")}
@@ -113,14 +134,20 @@ function EventsFourthScreen({ navigation }) {
       >
         2021 US Open
       </Text>
-      <Text>New York; August 30 - September 12, 2021</Text>
-      <Image
-        source={{
-          uri:
-            "https://echobrandgroup.com/wp-content/uploads/2016/07/USOPEN_LOGO_960X960_144DPI_SIDE_PANEL_SQ.jpg",
-        }}
-        style={{ height: 200, width: null, flex: 10 }}
-      />
+      <Card style={styles.card}>
+        <CardItem>
+          <Text>New York, USA: August 30 - September 12, 2021</Text>
+        </CardItem>
+        <CardItem cardBody>
+          <Image
+            source={{
+              uri:
+                "https://echobrandgroup.com/wp-content/uploads/2016/07/USOPEN_LOGO_960X960_144DPI_SIDE_PANEL_SQ.jpg",
+            }}
+            style={{ height: 400, width: null, flex: 10 }}
+          />
+        </CardItem>
+      </Card>
       <Button
         title="Next Event- Australia Open"
         onPress={() => navigation.navigate("Australia Open")}
@@ -141,3 +168,20 @@ export default function EventsScreen() {
     </Stack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  titleText: {
+    fontSize: 36,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 12,
+  },
+});
